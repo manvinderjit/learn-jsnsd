be62191a3b5f153a71a72a44e0104dc9163f48f4
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-06-11 14:56:58 -0400

    completed: 6-data-rest

---
+++ 6-data-rest/labs-6/routes/boat.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+let model = require('../model');
+
+router.get("/:id", function (req, res, next) { 
+  
+  const data = model.boat.read(req.params.id, (err, result) => {
+    if(err) {
+      if(err.message === 'not found') next();
+      else next(err);
+    } else{
+      res.send(result);
+    }          
+  });
+  
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  if(req.method === 'POST') {
+    if(req.header('content-type') === 'application/json'){
+      res.set('Content-Type', 'application/json');
+      var id = model.boat.uid();
+      model.boat.create(id, req.body.data, (err) => {
+        if(err) next(err);
+        else res.status(201).send({id});
+      })
+    }
+  }
+});
+
+router.delete('/:id', function (req, res, next) {
+  model.boat.del(req.params.id, (err) => {
+    if(err){
+      if(err.message === 'not found') next();
+      else next(err);
+    } else {
+      res.status(204).send();
+    }
+  })  
+});
+
+
+
+module.exports = router;
